f79ddce53df28ea65b2b743bda3d01932e16448e
josego85 <josego85@cdec75b6-5d2a-4a3c-36fd-0b72b1aa268f>
2010-05-23 17:12:44 +0000

    cree una nueva clase rectangulo queu extiende de la clase abstract figura

---
+++ PaintEvolution/src/Figuras/Rectangulo.java
@@ -0,0 +1,85 @@
+/**
+ * @(#)Rectangulo.java
+ *
+ * @author fires.
+ * @version 1.00 2010/5/23
+ */
+package Figuras;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Stroke;
+import java.awt.geom.Rectangle2D;
+
+/**
+ *
+ * Decalaracion de la clase Rectangulo que extiende de Figura
+ */
+public class Rectangulo extends Figura{
+    // Variables de clase
+    private int alto;           // alto del rectangulo
+    private int ancho;          // ancho del rectangulo
+
+    // Constructor
+    public Rectangulo(Punto punto, int alto, int ancho, Color colorBorde, Color colorRelleno,
+            int tamanhio){
+        super(punto, colorBorde, colorRelleno, tamanhio);
+        setAlto(alto);
+        setAncho(ancho);
+    }
+
+    // Setters y getters
+    /**
+     * Devuelve el alto del rectangulo
+     *
+     * @return alto el alto del rectangulo
+     */
+    public int getAlto() {
+        return alto;
+    }
+
+    /**
+     * Establece el alto del rectangulo
+     *
+     * @param alto el alto del rectangulo
+     */
+    public void setAlto(int alto) {
+        this.alto = alto;
+    }
+
+     /**
+     * Devuelve el ancho del rectangulo
+     *
+     * @return alto el ancho del rectangulo
+     */
+    public int getAncho() {
+        return ancho;
+    }
+
+    /**
+     * Establece el ancho del rectangulo
+     *
+     * @param alto el ancho del rectangulo
+     */
+    public void setAncho(int ancho) {
+        this.ancho = ancho;
+    }
+
+    // Metodos varios
+    // Sobre escribe el metodo dibujar
+    public void dibujar(Graphics g){
+        // Objetos
+        Graphics2D g2;
+        Rectangle2D r2;
+        Stroke bordeFigura;
+        r2 = new Rectangle2D.Float(getAlto(), getAncho(), getInicio().getCoordenadaX(),
+                getInicio().getCoordenadaY());
+        g2 = (Graphics2D)g;
+        bordeFigura = new BasicStroke(getTamanhio(), BasicStroke.CAP_ROUND, BasicStroke.JOIN_MITER);
+        g2.setColor(getColorBorde());
+        g2.setStroke(bordeFigura);
+        g2.draw(r2);
+    }
+}
