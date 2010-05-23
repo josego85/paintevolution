e8db24851b88746d42777375a767b934c514ca43
josego85 <josego85@cdec75b6-5d2a-4a3c-36fd-0b72b1aa268f>
2010-05-23 05:27:22 +0000

    Agrege informacion en la cabecera sobre licencias

--- PaintEvolution/src/figurasGeometricas/InterfaceFigura.java
+++ PaintEvolution/src/figurasGeometricas/InterfaceFigura.java
@@ -2,6 +2,9 @@
  * @(#)InterfaceFigura.java
  * @author Chuidiang
  * @co-author Modyfy fires
+ * sitio: http://www.chuidiang.com/
+ * licencia original de Chuidiang: Esta obra está bajo una licencia de Creative Commons.
+ * Mi licencia (fires): BSD
  * Fecha: 8/06/07 18:02
  */
 
