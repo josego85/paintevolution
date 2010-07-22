e5af5efcef342fed94a02d457b4ee83dd905c4c7
josego85 <josego85@cdec75b6-5d2a-4a3c-36fd-0b72b1aa268f>
2010-07-22 06:26:19 +0000

    nueva clase Ovalo

---
+++ PaintEvolution/src/Figuras/Ovalo.java
@@ -0,0 +1,59 @@
+/**
+ * @(#)Ovalo.java
+ *
+ * @author fires.
+ * @version 1.00 2010/7/22
+ */
+
+package Figuras;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Stroke;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
+
+/**
+ *
+ * Decalaracion de la clase Ovalo que extiende de Figura
+ */
+public class Ovalo extends Rectangulo{
+
+    // Constructor
+    public Ovalo(Point2D punto, int ancho, int alto, Color colorBorde, Color colorRelleno,
+            int tamanhio){
+        super(punto, ancho, alto, colorBorde, colorRelleno, tamanhio);
+    }
+
+    // Constructor
+    public Ovalo(int x, int y, int ancho, int alto, Color colorBorde, Color colorRelleno,
+            int tamanhio){
+        super(x, y, ancho, alto, colorBorde, colorRelleno, tamanhio);
+    }
+
+    // Metodos varios
+    // Sobre escribe el metodo dibujar
+    @Override
+    public void dibujar(Graphics g){
+        // Objetos
+        Graphics2D g2;
+        Ellipse2D e2;
+        Stroke bordeFigura;
+
+        // Si el color del relleno es null significa que no tiene relleno
+        if(getColorRelleno() != null){
+            g.setColor(getColorRelleno());
+            g.fillOval((int)getInicio().getX(), (int)getInicio().getY(),
+                    getAncho(), getAlto());
+        }
+        e2 = new Ellipse2D.Float((int)getInicio().getX(), (int)getInicio().getY(),
+                    getAncho(), getAlto());
+        g2 = (Graphics2D)g;
+        bordeFigura = new BasicStroke(getTamanhio(), BasicStroke.CAP_ROUND, BasicStroke.JOIN_MITER);
+        g2.setColor(getColorBorde());
+        g2.setStroke(bordeFigura);
+        g2.draw(e2);
+    }
+}
