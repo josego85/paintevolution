c6ce3aeb00c92b763e95d949a949604c4dd7c77c
josego85 <josego85@cdec75b6-5d2a-4a3c-36fd-0b72b1aa268f>
2010-08-23 04:20:39 +0000

    actualice la clase Constantes

--- PaintEvolution/src/Auxiliar/Constantes.java
+++ PaintEvolution/src/Auxiliar/Constantes.java
@@ -8,13 +8,13 @@ package Auxiliar;
 public class Constantes {
 
     // Constantes
-    public static final int MAXIMO_LARGO_PANTALLA_DIBUJO = 800;
+    public static final int MAXIMO_LARGO_PANTALLA_DIBUJO = 650;
     public static final int MAXIMO_ANCHO_PANTALLA_DIBUJO = 350;
     public static final int MINIMO_LARGO_PANTALLA_DIBUJO = 0;
     public static final int MINIMO_ANCHO_PANTALLA_DIBUJO = 0;
     public static final String TITULO_PROGRAMA = "Paint Evolution v1.0";
-    public static final int MARGEN_ANCHO_PANTALLA_DIBUJO = 150;
-    public static final int MARGEN_LARGO_PANTALLA_DIBUJO = 50;
+    public static final int MARGEN_ANCHO_PANTALLA_DIBUJO = 50;
+    public static final int MARGEN_LARGO_PANTALLA_DIBUJO = 120;
     public static final int MINIMO_GROSOR_BORDE = 1;
     public static final int MAXIMO_GROSOR_BORDE = 10;
 }
