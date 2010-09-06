47dd3816e66375924a5247e8c2b42879884893e0
josego85 <josego85@cdec75b6-5d2a-4a3c-36fd-0b72b1aa268f>
2010-09-06 07:28:39 +0000

    agregue Texto al panelDibujo

--- PaintEvolution/src/Auxiliar/Texto.java
+++ PaintEvolution/src/Auxiliar/Texto.java
@@ -1,6 +1,13 @@
 
 package Auxiliar;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.font.FontRenderContext;
+import java.awt.font.TextLayout;
+
 /**
  *
  * @author gaby
@@ -12,13 +19,30 @@ public class Texto {
     private int tipo;
     private int estilo;
     private int tamanio;
+    private int posicionInicialX;
+    private int posicionInicialY;
 
-    // Constructor por defecto
+
+    // Constructor con todos los valores
+    public Texto(String contenidoTexto, int tipo, int estilo, int tamanio, int posicionInicialX,
+            int posicionInicialY){
+        setContenidoTexto(contenidoTexto);
+        setTipo(tipo);
+        setEstilo(estilo);
+        setTamanio(tamanio);
+        setPosicionInicialX(posicionInicialX);
+        setPosicionInicialY(posicionInicialX);
+    }
+
+    // Constructor sin las posiciones iniciales X e Y
+    // Se coloca las posicionInicialX e posicionInicialY con valor 0
     public Texto(String contenidoTexto, int tipo, int estilo, int tamanio){
         setContenidoTexto(contenidoTexto);
         setTipo(tipo);
         setEstilo(estilo);
-        setTamaño(tamanio);
+        setTamanio(tamanio);
+        setPosicionInicialX(0);
+        setPosicionInicialY(0);
     }
 
 
@@ -39,11 +63,11 @@ public class Texto {
         this.estilo = estilo;
     }
 
-    public int getTamaño() {
+    public int getTamanio() {
         return tamanio;
     }
 
-    public void setTamaño(int tamanio) {
+    public void setTamanio(int tamanio) {
         this.tamanio = tamanio;
     }
 
@@ -53,5 +77,38 @@ public class Texto {
 
     public void setTipo(int tipo) {
         this.tipo = tipo;
+    }
+
+    public int getPosicionInicialX() {
+        return posicionInicialX;
+    }
+
+    public void setPosicionInicialX(int posicionInicialX) {
+        this.posicionInicialX = posicionInicialX;
+    }
+
+    public int getPosicionInicialY() {
+        return posicionInicialY;
+    }
+
+    public void setPosicionInicialY(int posicionInicialY) {
+        this.posicionInicialY = posicionInicialY;
+    }
+
+
+    // Metodos varios
+    public void dibujar(Graphics g){
+        // Objetos
+        Graphics2D g2;
+        FontRenderContext contextoFuente;
+        Font fuente;
+        TextLayout layout;
+
+        g2 = (Graphics2D)g;
+        contextoFuente = g2.getFontRenderContext();
+        fuente = new Font( "Times", getEstilo(), getTamanio());
+        layout = new TextLayout(getContenidoTexto(), fuente, contextoFuente );
+        g2.setColor( Color.red );
+        layout.draw( g2,getPosicionInicialX(), getPosicionInicialY());
     }
 }
