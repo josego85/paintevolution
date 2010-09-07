88f92ea7fcb532fdbd86b584697f8c983846daa7
josego85 <josego85@cdec75b6-5d2a-4a3c-36fd-0b72b1aa268f>
2010-09-07 07:41:24 +0000

    agregue 4 nuevas constantes para el SpinnerNumberModel

--- trunk/PaintEvolution/src/Auxiliar/Constantes.java
+++ trunk/PaintEvolution/src/Auxiliar/Constantes.java
@@ -17,4 +17,8 @@ public class Constantes {
     public static final int MARGEN_LARGO_PANTALLA_DIBUJO = 120;
     public static final int MINIMO_GROSOR_BORDE = 1;
     public static final int MAXIMO_GROSOR_BORDE = 10;
+    public static final int VALOR_PREDETERMINADO_SPINNER_NUMBER_MODEL = 36;
+    public static final int VALOR_MINIMO_SPINNER_NUMBER_MODEL = 6;
+    public static final int VALOR_MAXIMO_SPINNER_NUMBER_MODEL = 150;
+    public static final int INCREMENTO_SPINNER_NUMBER_MODEL = 1;
 }
