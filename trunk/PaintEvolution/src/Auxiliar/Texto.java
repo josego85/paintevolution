2e136bed857b26c8609ef38fa22c74eee76dcdc7
josego85 <josego85@cdec75b6-5d2a-4a3c-36fd-0b72b1aa268f>
2010-10-11 07:30:52 +0000

    Agregue los comentarios javadoc

--- trunk/PaintEvolution/src/Auxiliar/Texto.java
+++ trunk/PaintEvolution/src/Auxiliar/Texto.java
@@ -1,4 +1,3 @@
-
 package Auxiliar;
 
 import java.awt.Color;
@@ -9,21 +8,78 @@ import java.awt.font.FontRenderContext;
 import java.awt.font.TextLayout;
 
 /**
+ *@(#)Texto.java
  *
- * @author gaby
+ * @author fires
+ * @version 1.00
+ * @since 1.6
+ */
+/**
+ * Clase Texto para poder manupular texto.
+ * @since 1.6
  */
 public class Texto {
-    // Variables
+    ////////////////////////////////////////////////////////////////////////////
+    // Variables de clase
+    ////////////////////////////////////////////////////////////////////////////
+    /**
+     * El contenido del texto.
+     * @since 1.6
+     */
     private String contenidoTexto;
+
+    /**
+     * El tipó de texto.
+     * @since 1.6
+     */
     private String tipo;
+
+    /**
+     * El estilo del texto.
+     * @since 1.6
+     */
     private int estilo;
+
+    /**
+     * El tamanhio del texto.
+     * @since 1.6
+     */
     private int tamanio;
+
+    /**
+     * La posicion inicial de la coordenada x.
+     * @since 1.6
+     */
     private int posicionInicialX;
+
+    /**
+     * La posicion inicial de la coordenada y.
+     * @since 1.6
+     */
     private int posicionInicialY;
+
+    /**
+     * El color del texto.
+     * @since 1.6
+     */
     private Color color;
 
 
-    // Constructor con todos los valores
+    ////////////////////////////////////////////////////////////////////////////
+    // Constructores
+    ////////////////////////////////////////////////////////////////////////////
+    /**
+     * Construye un Texto.
+     *
+     * @param contenidoTexto El contenido
+     * @param tipo El tipo
+     * @param estilo El estilo
+     * @param tamanio El tamanhio
+     * @param color El color
+     * @param posicionInicialX La coordenada inicial x
+     * @param posicionInicialY La coordenada inicial y
+     * @since 1.6
+     */
     public Texto(String contenidoTexto, String tipo, int estilo, int tamanio,
             Color color, int posicionInicialX, int posicionInicialY){
         setContenidoTexto(contenidoTexto);
@@ -35,8 +91,18 @@ public class Texto {
         setPosicionInicialY(posicionInicialX);
     }
 
-    // Constructor sin las posiciones iniciales X e Y
-    // Se coloca las posicionInicialX e posicionInicialY con valor 0
+    /**
+     * Construye un Texto.
+     * Se coloca la posicion inicial x es igual a 0.
+     * Se coloca la posicion inicial y es igual a 0.
+     * 
+     * @param contenidoTexto El contenido 
+     * @param tipo El tipo
+     * @param estilo El estilo
+     * @param tamanio El tamanhio
+     * @param color El color
+     * @since 1.6
+     */
     public Texto(String contenidoTexto, String tipo, int estilo, int tamanio,
             Color color){
         setContenidoTexto(contenidoTexto);
@@ -49,65 +115,146 @@ public class Texto {
     }
 
 
-    // Metodos get y set
+    ////////////////////////////////////////////////////////////////////////////
+    // Setters y Getters
+    ////////////////////////////////////////////////////////////////////////////
+    /**
+     * Devuelve el contenido del texto.
+     *
+     * @return El contenido del texto
+     * @since 1.6
+     */
     public String getContenidoTexto() {
         return contenidoTexto;
     }
 
+    /**
+     * Estabelce el contenido del texto.
+     *
+     * @param contenidoTexto El contenido del texto
+     * @since 1.6
+     */
     public void setContenidoTexto(String contenidoTexto) {
         this.contenidoTexto = contenidoTexto;
     }
 
+    /**
+     * Devuelve el estilo del texto.
+     *
+     * @return El estilo delo texto.
+     */
     public int getEstilo() {
         return estilo;
     }
 
+    /**
+     * Establece el estilo del texto.
+     *
+     * @param estilo El estilo del texto
+     */
     public void setEstilo(int estilo) {
         this.estilo = estilo;
     }
 
+    /**
+     * Devuelve el tamanhio del texto.
+     *
+     * @return El tamanhio del texto.
+     */
     public int getTamanio() {
         return tamanio;
     }
 
+    /**
+     * Estblece el tamanhio del texto.
+     *
+     * @param tamanio El tamanhio del texto
+     */
     public void setTamanio(int tamanio) {
         this.tamanio = tamanio;
     }
 
+    /**
+     * Devuelve el tipo del texto.
+     *
+     * @return El tipo de texto.
+     */
     public String getTipo() {
         return tipo;
     }
 
+    /**
+     * Establece el tipo de texto.
+     *
+     * @param tipo El tipo de texto.
+     */
     public void setTipo(String tipo) {
         this.tipo = tipo;
     }
 
+    /**
+     * Devuelve la posicion inicial de x.
+     *
+     * @return La posicion inicial de x
+     */
     public int getPosicionInicialX() {
         return posicionInicialX;
     }
 
+    /**
+     * Establece la posicion inicial de x.
+     *
+     * @param posicionInicialX La posicion inicial de x
+     */
     public void setPosicionInicialX(int posicionInicialX) {
         this.posicionInicialX = posicionInicialX;
     }
 
+    /**
+     * Devuelve la posicion inicial de y.
+     *
+     * @return la posicion inicial de y.
+     */
     public int getPosicionInicialY() {
         return posicionInicialY;
     }
 
+    /**
+     * Establece la posicion inicial de y.
+     *
+     * @param posicionInicialY La posicion inicial de y
+     */
     public void setPosicionInicialY(int posicionInicialY) {
         this.posicionInicialY = posicionInicialY;
     }
 
+    /**
+     * Devuelve el color del texto.
+     *
+     * @return El color del texto.
+     */
     public Color getColor() {
         return color;
     }
 
+    /**
+     * Establece el color del texto.
+     *
+     * @param color El color del texto
+     */
     public void setColor(Color color) {
         this.color = color;
     }
 
 
+    ////////////////////////////////////////////////////////////////////////////
     // Metodos varios
+    ////////////////////////////////////////////////////////////////////////////
+    /**
+     * Dibuja el texto.
+     * @param g El objeto Graphics
+     * @since 1.6
+     */
     public void dibujar(Graphics g){
         // Objetos
         Graphics2D g2;
