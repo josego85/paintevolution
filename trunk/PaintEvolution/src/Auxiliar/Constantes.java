f85a16f62c2d152504dd37489ab39466892c4099
josego85 <josego85@cdec75b6-5d2a-4a3c-36fd-0b72b1aa268f>
2010-10-11 06:30:45 +0000

    Agregue los comentarios javadoc

--- trunk/PaintEvolution/src/Auxiliar/Constantes.java
+++ trunk/PaintEvolution/src/Auxiliar/Constantes.java
@@ -1,15 +1,27 @@
 package Auxiliar;
 
 /**
+ *@(#)Constantes.java
  *
  * @author fires
+ * @version 1.00
+ * @since 1.6
  */
-// Clase que aparecen todas las constantes del Programa Paint Evolution
+/**
+ * Clase que aparecen todas las constantes del Programa Paint Evolution
+ * @since 1.6
+ */
 public class Constantes {
-    // De titulos
+    /**
+     * Todos los titulos.
+     * @since 1.6
+     */
     public static final String TITULO_PROGRAMA = "Paint Evolution v1.0";
 
-    // Del Panel Dibujo
+    /**
+     * Del Panel Dibujo.
+     * @since 1.6
+     */
     public static final int MAXIMO_LARGO_PANTALLA_DIBUJO = 1024;
     public static final int MAXIMO_ANCHO_PANTALLA_DIBUJO = 1024;
     public static final int MINIMO_LARGO_PANTALLA_DIBUJO = 0;
@@ -17,17 +29,26 @@ public class Constantes {
     public static final int MARGEN_ANCHO_PANTALLA_DIBUJO = 50;
     public static final int MARGEN_LARGO_PANTALLA_DIBUJO = 120;
 
-    // De rango de valores del grosor del borde
+    /**
+     * De rango de valores del grosor del borde.
+     * @since 1.6
+     */
     public static final int MINIMO_GROSOR_BORDE = 1;
     public static final int MAXIMO_GROSOR_BORDE = 10;
 
-    // De texto
+    /**
+     * De texto.
+     * @since 1.6
+     */
     public static final int VALOR_PREDETERMINADO_SPINNER_NUMBER_MODEL = 36;
     public static final int VALOR_MINIMO_SPINNER_NUMBER_MODEL = 6;
     public static final int VALOR_MAXIMO_SPINNER_NUMBER_MODEL = 150;
     public static final int INCREMENTO_SPINNER_NUMBER_MODEL = 1;
     public static final String TIPO_FUENTE_LETRA_PREDETERMINADO = "Dialog";
 
-    // Varios
+    /**
+     * Varios.
+     * @since 1.6
+     */
     public static final String INCREMENTO_CANTIDAD_DE_ESPACIO_TITULO = "     ";
 }
