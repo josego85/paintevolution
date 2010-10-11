d2cf9b43beabbc3a073795b4f758bcddd932dbfc
josego85 <josego85@cdec75b6-5d2a-4a3c-36fd-0b72b1aa268f>
2010-10-11 05:46:04 +0000

    Agregue los comentarios javadoc

--- PaintEvolution/src/Figuras/FiguraConRelleno.java
+++ PaintEvolution/src/Figuras/FiguraConRelleno.java
@@ -8,48 +8,85 @@ import java.awt.geom.Point2D;
  *
  * @author fires.
  * @version 1.00 2010/8/22
+ * @since 1.6
  */
 /**
  *
- * Decalaracion de la clase abstracta Figura con relleno.
- * Esta clase extiende de figura pero agrega color de relleno
+ * Clase abstracta Figura con relleno que extiende de Figura.
+ * Agrega color de relleno.
  */
 public abstract class FiguraConRelleno extends Figura{
-    // Variables
-    private Color colorRelleno;         // Color del relleno de la figura
+    ////////////////////////////////////////////////////////////////////////////
+    // Variables de clase
+    ////////////////////////////////////////////////////////////////////////////
+    /**
+     * Color del relleno de FiguraConRelleno.
+     * @since 1.6
+     */
+    private Color colorRelleno;         
 
-    // Constructor Predeterminado
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Constructores
+    ////////////////////////////////////////////////////////////////////////////
+    /**
+     * Es una clase abstracta y no se puede instanciar.<br>
+     * Su color de relleno predeterminado es blanco.
+     * @since 1.6
+     */
     public FiguraConRelleno(){
         super();
-        setColorRelleno(Color.WHITE);           // ColorRelleno blanco
+        setColorRelleno(Color.WHITE);           
     }
 
+    /**
+     * Es una clase abstracta y no se puede instanciar.<br>
+     *
+     * @param punto Las coordenadas de inicio (x, y) del figura con relleno
+     * @param colorBorde El color del borde
+     * @param colorRelleno El color del relleno
+     * @param tamanhio El tamañio del borde
+     * @since 1.6
+     */
     public FiguraConRelleno(Point2D punto, Color colorBorde, Color colorRelleno,
              int tamanhio){
         super(punto, colorBorde, tamanhio);
         setColorRelleno(colorRelleno);  // ColorRelleno
     }
 
+    /**
+     * Es una clase abstracta y no se puede instanciar.<br>
+     *
+     * @param x La coordenada x de la figura con relleno
+     * @param y La coordenada y de la figura con relleno
+     * @param colorBorde El color del borde
+     * @param colorRelleno El color del relleno
+     * @param tamanhio El tamañio del borde
+     * @since 1.6
+     */
     public FiguraConRelleno(int x, int y, Color colorBorde, Color colorRelleno,
              int tamanhio){
         super(x, y, colorBorde, tamanhio);
         setColorRelleno(colorRelleno);              // ColorRelleno
     }
 
+
+     ////////////////////////////////////////////////////////////////////////////
     // Setters y Getters
+    ////////////////////////////////////////////////////////////////////////////
     /**
-     * Devuelve el color del relleno
+     * Devuelve el color del relleno.
      *
-     * @return colorRelleno el color del relleno
+     * @return colorRelleno El color del relleno
      */
     public Color getColorRelleno() {
         return colorRelleno;
     }
 
     /**
-     * Establece el color del relleno
+     * Establece el color del relleno.
      *
-     * @param colorRelleno el color del relleno
+     * @param colorRelleno El color del relleno
      */
     public void setColorRelleno(Color colorRelleno) {
         this.colorRelleno = colorRelleno;
