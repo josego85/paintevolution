78136e102fde8c5a363ccdca0296e0a334f85ee5
josego85 <josego85@cdec75b6-5d2a-4a3c-36fd-0b72b1aa268f>
2010-10-11 07:08:32 +0000

    Agregue los comentarios javadoc

--- trunk/PaintEvolution/src/Auxiliar/FiltroArchivo.java
+++ trunk/PaintEvolution/src/Auxiliar/FiltroArchivo.java
@@ -3,13 +3,25 @@ package Auxiliar;
 import java.io.File;
 import javax.swing.filechooser.FileFilter;
 
-
-
 /**
+ *@(#)FiltroArchivo.java
  *
  * @author fires
+ * @version 1.00
+ * @since 1.6
+ */
+/**
+ * Clase Filtro de los archivos. Solo filtra archivos *.png.
+ * @since 1.6
  */
 public class FiltroArchivo extends FileFilter {
+    /**
+     * Para saber si el archivo de entrada tiene un formato png.
+     * 
+     * @param archivo El archivo de entrada
+     * @return True si la extension final es png
+     * @since 1.6
+     */
     public boolean accept(File archivo) {
         if (archivo.isDirectory()) {
             return true;
@@ -29,8 +41,13 @@ public class FiltroArchivo extends FileFilter {
         return false;
     }
 
+    /**
+     * Devuelve siempre la extension png.
+     *
+     * @return Devuelve *.png
+     * @since 1.6
+     */
     public String getDescription() {
         return "*.png";
     }
-
 }
