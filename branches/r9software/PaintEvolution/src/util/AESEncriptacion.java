a297abebe3ff40f5fd77f937ea9e48ebe6f5e939
josego85@gmail.com <josego85@gmail.com@cdec75b6-5d2a-4a3c-36fd-0b72b1aa268f>
2013-03-08 04:32:09 +0000

    Se cambio de paquete para los algoritmos

--- branches/r9software/PaintEvolution/src/util/AESEncriptacion.java
+++ branches/r9software/PaintEvolution/src/util/AESEncriptacion.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package Pruebas;
+package util;
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
